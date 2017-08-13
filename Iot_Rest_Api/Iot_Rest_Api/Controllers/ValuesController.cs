3dd5f32c653fcd08ca4ed24f455c351d3fd5836b
Ishanka Lakshan <isankalakshan@gmail.com>
2017-08-13 22:11:37 +0530

    initial commit

---
+++ Iot_Rest_Api/Iot_Rest_Api/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iot_Rest_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        private DataRepository _datarepository; 
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{status}")]
+        public string UpdateMeetingRoomStatus(string status)
+        {
+            _datarepository = new DataRepository();
+            var result = GetIdAndStatusSeperated(status);
+            _datarepository.UpdateMeetingRoomStatus(result[0], result[1]);
+            return "done";
+        }
+
+        private int[] GetIdAndStatusSeperated(string input)
+        {
+            string[] words = input.Split('-');
+            var id = Convert.ToInt16(words[0]);
+            var status = Convert.ToInt16(words[1]);
+            return new int[] { id,status};
+        }
+    }
+}
---
+++ Iot_Rest_Api/Iot_Rest_Api/DataRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace Iot_Rest_Api
+{
+    public class DataRepository
+    {
+        private const string _connectionString = "Server=tcp:iit-msc-common-server.database.windows.net,1433;Initial Catalog=db-iot;Persist Security Info=False;User ID=dbo.admin;Password=Intel@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection OpenDbConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        private void Update(string sql, object parameters = null)
+        {
+            using (var db = OpenDbConnection())
+            {
+                var result = db.Query(sql, parameters);
+            }
+        }
+
+        public void UpdateMeetingRoomStatus(int id,int status)
+        {
+            var queryArgs = new
+            {
+                id,status
+            };
+            var sql = "UPDATE [dbo].[MeetingRoom] SET Status=@status WHERE Id=@id";
+            Update(sql, queryArgs);
+        }
+
+    }
+}
